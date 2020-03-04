af4e56284a0485acbd64e7d33647a45e2eadea1d
brian <brian@207.23.189.225>
2020-03-03 20:51:17 -0800

    test class, will delete

---
+++ main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Maven!");
+	}
+}
