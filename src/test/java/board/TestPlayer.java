02cc847d62abb2790e9419e7d9e2d6a35eeac600
Vera <veray@sfu.ca>
2020-03-30 18:45:50 -0700

    Added Unit test dependency to POM file

---
+++ src/test/java/board/TestPlayer.java
@@ -0,0 +1,16 @@
+package board;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestPlayer {
+
+	@Test
+    void justAnExample() {
+    	System.out.println("Testing Player");
+    }
+
+	
+}
